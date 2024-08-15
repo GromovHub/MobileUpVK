958b3a8e333e4ee65e57dd7333f1ca215877f965
Vitaly Gromov <gromovitaly@gmail.com>
2024-08-15 21:48:27 +0300

    Initial Commit

---
+++ MobileUpVK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  Project name: MobileUpVK
+//  File name: ViewController.swift
+//
+//  Copyright © Gromov V.O., 2024
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MobileUpVKTests/MobileUpVKTests.swift
@@ -0,0 +1,18 @@
+//
+//  Project name: MobileUpVK
+//  File name: MobileUpVKTests.swift
+//
+//  Copyright © Gromov V.O., 2024
+//
+
+
+import Testing
+@testable import MobileUpVK
+
+struct MobileUpVKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
